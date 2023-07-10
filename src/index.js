1eb3bcd0fea90735b8c033e0a493d4278da6db3b
Milli-saved <million12.tenkir@gmail.com>
2023-07-10 11:32:57 +0300

    Add step2 to the project

--- src/index.js
+++ src/index.js
@@ -26,4 +26,32 @@ app.get("/step1", (req, res) => {
     });
 });
 
+app.get("/step2", (req, res) => {
+  if (status.step1) {
+    let data = {
+      tillCode: "005",
+    };
+    const header = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post(baseURL + "CheckMe", data, { header })
+      .then((response) => {
+        status.step2 = true;
+        res.status(200).json(response.data);
+      })
+      .catch((error) => {
+        res.status(400).json(error);
+      });
+  } else {
+    res.status(200).json({
+      msg: "Step 1 is false.",
+    });
+  }
+});
+
+app.get("/step3", (req, res) => {
+    
+})
+
 app.listen(4006, () => console.log("Server is running on port 4005"));
