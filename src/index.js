1492be3ee873f97ff97269050aa0d0fd0460b26d
Milli-saved <million12.tenkir@gmail.com>
2023-07-10 11:22:18 +0300

    Add status state to the project.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@ app.use(cors());
 
 baseURL = "https://cbebirrpaymentgateway.cbe.com.et:8888/api/cbebpg/";
 
+let status = {
+  step1: false,
+  step2: false,
+  step3: false,
+  step4: false,
+};
+
 app.get("/step1", (req, res) => {
   axios
     .get(baseURL + "CheckCall")
     .then((response) => {
+      status.step1 = true;
       res.status(200).json(response.data);
     })
     .catch((error) => {
