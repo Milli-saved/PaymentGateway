18c26e24ec512095d0ce5db69aa9daec8b05d3a5
Milli-saved <million12.tenkir@gmail.com>
2023-07-12 10:49:41 +0300

    Add step2 function in paymentService

--- client/src/paymentFeatures/paymentService.js
+++ client/src/paymentFeatures/paymentService.js
@@ -3,15 +3,28 @@ const BaseURL =
   "https://cors-anywhere.herokuapp.com/https://cbebirrpaymentgateway.cbe.com.et:8888/api/cbebpg/";
 
 const step1 = async () => {
-    let header = {
-        "Content-Type": "application/xml",
-        changeOrigin : true
-    }
+  let header = {
+    "Content-Type": "application/xml",
+    changeOrigin: true,
+  };
   const response = await axios.get(BaseURL + "CheckCall", header);
   return response.data;
 };
 
+const step2 = async () => {
+  let header = {
+    "Content-Type": "application/xml",
+    changeOrigin: true,
+  };
+  let data = {
+    tillCode: "005",
+  };
+    const response = await axios.post(BaseURL + "CheckMe", data, { header });
+    return response.data
+};
+
 const paymentService = {
   step1,
+  step2,
 };
 export default paymentService;
