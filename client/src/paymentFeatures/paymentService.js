fb7ea158af5f26b3d0b44cafef7e026de7e2b1fe
Milli-saved <million12.tenkir@gmail.com>
2023-07-12 10:27:14 +0300

    Add step1 function to payment services

---
+++ client/src/paymentFeatures/paymentService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const BaseURL = "https://cbebirrpaymentgateway.cbe.com.et:8888/api/cbebpg/";
+
+const step1 = async () => {
+  // axios
+  //   .get(baseURL + "CheckCall")
+  //   .then((response) => {
+  //     status.step1 = true;
+  //     res.status(200).json(response.data);
+  //     console.log("the status is", response.data);
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).json(error);
+  //   });
+  const response = await axios.get(BaseURL + "CheckCall");
+  return response.data;
+};
