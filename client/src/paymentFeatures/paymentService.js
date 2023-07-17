0b2fef36490fb5e11e550e8eab114b0d85f4d3c8
Milli-saved <million12.tenkir@gmail.com>
2023-07-17 14:06:59 +0300

    Step 3 working properly and returning MName after decryption.

--- client/src/paymentFeatures/paymentService.js
+++ client/src/paymentFeatures/paymentService.js
@@ -26,91 +26,85 @@ const step2 = async () => {
 const step3 = async () => {
   let iv = CryptoJS.enc.Hex.parse("0000000000000000");
   let key = CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916");
-  let encryptedMsg = CryptoJS.AES.encrypt("CBE_School_Fee", key, {
-    iv,
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7,
-  }).toString();
 
   const toEncryptedArray = CryptoJS.enc.Utf8.parse("CBE_School_Fee");
-  const ivv = CryptoJS.lib.WordArray.random(8); // Generate a random IV of 64 bits (8 bytes)
-  const securityKeyArray = CryptoJS.MD5(CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916"));
-  const objTripleDESCryptoService = CryptoJS.TripleDES.encrypt(toEncryptedArray, securityKeyArray, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7,
-    ivv
-  });
-  
-  const value = objTripleDESCryptoService.toString()
+  const ivv = CryptoJS.lib.WordArray.random(8);
+  const securityKeyArray = CryptoJS.MD5(
+    CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916")
+  );
+  const objTripleDESCryptoService = CryptoJS.TripleDES.encrypt(
+    toEncryptedArray,
+    securityKeyArray,
+    {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+      ivv,
+    }
+  );
 
+  const value = objTripleDESCryptoService.toString();
 
-
-    console.log("the newly returned: ", objTripleDESCryptoService.toString())
-    
-    // console.log("the newly returned Dec: ", objTripleDESCryptoServiceDec.toString() == '')
+  console.log("the newly returned: ", objTripleDESCryptoService.toString());
 
   let header = {
     "Content-Type": "application/xml",
     changeOrigin: true,
   };
-  console.log("the encrypted message: ", encryptedMsg);
   let data = {
     MName: "CBE_School_Fee",
     tillCode: "005",
   };
   const response = await axios.post("/api/cbebpg/CheckMeENC", data, header);
 
-  console.log(response.data.slice(3));
   let new_data = response.data.slice(3);
 
-  const toDecryptArray = new_data;
-  console.log("tod arr: ", toDecryptArray)
-  const securityKeyArrayDec = CryptoJS.MD5(CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916"));
+  // const securityKeyArrayDec = CryptoJS.MD5(
+  //   CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916")
+  // );
   const objTripleDESCryptoServiceDec = CryptoJS.TripleDES.decrypt(
-    { value: toDecryptArray },
-    securityKeyArrayDec,
+    new_data,
+    securityKeyArray,
     {
       mode: CryptoJS.mode.ECB,
       padding: CryptoJS.pad.Pkcs7,
-      ivv
+      ivv,
     }
-    );
-    console.log("the newly returned Dec TYPE: ",typeof objTripleDESCryptoServiceDec.toString())
-    console.log("the newly returned Dec: ", objTripleDESCryptoServiceDec.toString() == '')
+  );
+  let decryptedWord = CryptoJS.enc.Utf8.stringify(objTripleDESCryptoServiceDec);
 
-  // const decryptedMsg = CryptoJS.AES.decrypt(new_data, key, {
-  //   iv,
-  //   mode: CryptoJS.mode.ECB,
-  //   padding: CryptoJS.pad.Pkcs7,
-  // }).toString(CryptoJS.enc.Utf8);
-
-  // console.log("decryptede message: ", decryptedMsg);
-  //
-  
-
-  console.log(objTripleDESCryptoServiceDec.toString());
-  console.log(CryptoJS.enc.Utf8.stringify(objTripleDESCryptoServiceDec));
-  console.log(response.data);
+  console.log(
+    "the decryption process *****************: stringify: ",
+    CryptoJS.enc.Utf8.stringify(objTripleDESCryptoServiceDec)
+  );
+  return decryptedWord;
 };
-
-// Encrypt plaintext using TripleDES with ECB mode and PKCS7 padding
- 
 
 
 const step4 = async () => {
-  let iv = CryptoJS.enc.Hex.parse("0000000000000000");
-  let key = CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916");
-  let encryptedMsg = CryptoJS.AES.encrypt("CBE_School_Fee", key, {
-    iv,
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7,
-  }).toString();
   let header = {
     "Content-Type": "application/xml",
     changeOrigin: true,
   };
+
+  const toEncryptedArray = CryptoJS.enc.Utf8.parse("CBE_School_Fee");
+  const ivv = CryptoJS.lib.WordArray.random(8);
+  const securityKeyArray = CryptoJS.MD5(
+    CryptoJS.enc.Utf8.parse("b14ca5898a4e4133bbce2ea2315a1916")
+  );
+  const objTripleDESCryptoService = CryptoJS.TripleDES.encrypt(
+    toEncryptedArray,
+    securityKeyArray,
+    {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+      ivv,
+    }
+  );
+
+  const value = objTripleDESCryptoService.toString();
+  console.log("the value of value in S4: ", value);
   const data = {
-    MName: encryptedMsg,
+    MName: value,
     tillCode: "005",
   };
   const response = await axios.post("/api/cbebpg/CheckMeDEC", data, header);
